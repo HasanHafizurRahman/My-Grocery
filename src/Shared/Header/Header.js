6a51e6b02e19d313f35702dd62dabe819b66eb2a
HasanHafizurRahman <hasanshanto922@gmail.com>
2022-04-30 03:44:15 +0600

    banner added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#features">About us</Nav.Link>
+      <Nav.Link href="#pricing">All Items</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
